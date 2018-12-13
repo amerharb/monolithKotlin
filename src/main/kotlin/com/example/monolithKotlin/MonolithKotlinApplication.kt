9907f2844f010d5315775a8a7b972b7a608b30b3
amerharb <amer.harb@gmail.com>
2018-12-13 13:40:36 +0100

    init

---
+++ src/main/kotlin/com/example/monolithKotlin/MonolithKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.example.monolithKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonolithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonolithKotlinApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/example/monolithKotlin/MonolithKotlinApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.example.monolithKotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MonolithKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
